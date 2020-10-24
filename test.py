22cab2e8ed09fac59fb4b96241541551e3d7edcb
CPALyth <160548528@qq.com>
2020-10-24 18:59:38 +0800

    09

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-import os
-print(__file__)
-print(os.path.abspath(__file__))
-print(os.getcwd())
+import numpy as np
 
-for root, dirs, files in os.walk("F:/Study/NCK"):
-    print("root:", root)
-    print("dirs:", dirs)
-    print("files:", files)
-    for name in files:
-        print(os.path.join(root, name))
-    for name in dirs:
-        print(os.path.join(root, name))
+# 参数意思分别 是从a 中以概率P，随机选择3个, p没有指定的时候相当于是一致的分布
+a1 = np.random.choice(a=5, size=3, replace=False, p=None)
+print(a1)
+# 非一致的分布，会以多少的概率提出来
+a2 = np.random.choice(a=5, size=3, replace=False, p=[0.2, 0.1, 0.3, 0.4, 0.0])
+print(a2)
+# replacement 代表的意思是抽样之后还放不放回去，如果是False的话，那么出来的三个数都不一样
