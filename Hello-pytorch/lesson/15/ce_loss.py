d4e52e85679d091aa0662db636d00edba7b409a2
CPALyth <160548528@qq.com>
2020-11-01 10:06:23 +0800

    15

---
+++ Hello-pytorch/lesson/15/ce_loss.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from matplotlib import pyplot as plt
+import numpy as np
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+hello_pytorch_dir = os.path.join(os.path.dirname(__file__), "..", "..")
+sys.path.append(hello_pytorch_dir)
+
+from model.lenet import LeNet
+from tools.my_dataset import RMBDataset
+from tools.common_tools import transform_invert, set_seed
+
+set_seed(1)
+rmb_label = {"1": 0, "100": 1}
+
+MAX_EPOCH = 10
+BATCH_SIZE = 16
+LR = 0.01
+log_interval = 10
+val_interval = 1
+
+# ============================ step 1/5 数据 ============================
+dir_split = os.path.join(hello_pytorch_dir, "data", "rmb_split")
+dir_train = os.path.join(dir_split, "train")
+dir_valid = os.path.join(dir_split, "valid")
+
+norm_mean = [0.485, 0.456, 0.406]
+norm_std = [0.229, 0.224, 0.225]
+
+train_transform = transforms.Compose([
+    transforms.Resize((32,32)),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomGrayscale(p=0.8),
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std),
+])
+
+valid_transform = transforms.Compose([
+    transforms.Resize((32,32)),
+    transforms.ToTensor(),
+    transforms.Normalize(norm_mean, norm_std),
+])
+
+train_data = RMBDataset(data_dir=dir_train, transform=train_transform)
+valid_data = RMBDataset(data_dir=dir_valid, transform=valid_transform)
+
+# 构建DataLoder
+train_loader = DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+valid_loader = DataLoader(dataset=valid_data, batch_size=BATCH_SIZE)
+
+# ============================ step 2/5 模型 ============================
+net = LeNet(classes=2)
+net.initialize_weights()
+
+# ============================ step 3/5 损失函数 ============================
+loss_func = nn.CrossEntropyLoss()  # 选择损失函数
+
+# ============================ step 4/5 优化器 ============================
+optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9)                        # 选择优化器
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)     # 设置学习率下降策略
+
+# ============================ step 5/5 训练 ============================
+train_curve = list()
+valid_curve = list()
+
+for epoch in range(MAX_EPOCH):
+
+    loss_mean = 0.
+    correct = 0.
+    total = 0.
+
+    net.train()
+    for i, data in enumerate(train_loader):
+
+        # forward
+        inputs, labels = data
+        outputs = net(inputs)
+
+        # backward
+        optimizer.zero_grad()
+        loss = loss_func(outputs, labels)
+        loss.backward()
+
+        # update weights
+        optimizer.step()
+
+        # 统计分类情况
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).squeeze().sum().numpy()
+
+        # 打印训练信息
+        loss_mean += loss.item()
+        train_curve.append(loss.item())
+        if (i+1) % log_interval == 0:
+            loss_mean = loss_mean / log_interval
+            print("Training:Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] Loss: {:.4f} Acc:{:.2%}".format(
+                epoch, MAX_EPOCH, i+1, len(train_loader), loss_mean, correct / total))
+            loss_mean = 0.
+
+    scheduler.step()  # 更新学习率
+
+    # validate the model
+    if (epoch+1) % val_interval == 0:
+
+        correct_val = 0.
+        total_val = 0.
+        loss_val = 0.
+        net.eval()
+        with torch.no_grad():
+            for j, data in enumerate(valid_loader):
+                inputs, labels = data
+                outputs = net(inputs)
+                loss = loss_func(outputs, labels)
+
+                _, predicted = torch.max(outputs.data, 1)
+                total_val += labels.size(0)
+                correct_val += (predicted == labels).squeeze().sum().numpy()
+
+                loss_val += loss.item()
+
+            valid_curve.append(loss_val)
+            print("Valid:\t Epoch[{:0>3}/{:0>3}] Iteration[{:0>3}/{:0>3}] Loss: {:.4f} Acc:{:.2%}".format(
+                epoch, MAX_EPOCH, j+1, len(valid_loader), loss_val, correct / total))
+
+
+train_x = range(len(train_curve))
+train_y = train_curve
+
+train_iters = len(train_loader)
+valid_x = np.arange(1, len(valid_curve)+1) * train_iters*val_interval # 由于valid中记录的是epochloss，需要对记录点进行转换到iterations
+valid_y = valid_curve
+
+plt.plot(train_x, train_y, label='Train')
+plt.plot(valid_x, valid_y, label='Valid')
+
+plt.legend(loc='upper right')
+plt.ylabel('loss value')
+plt.xlabel('Iteration')
+plt.show()
